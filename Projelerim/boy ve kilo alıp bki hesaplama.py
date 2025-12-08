39abaf6c9da74b2e5c9bf134110ee3aeb179fe47
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-08 20:57:19 +0300

    yeni proje (bki hesaplama) eklendi.

---
+++ Projelerim/boy ve kilo alıp bki hesaplama.py
@@ -0,0 +1,24 @@
+boy = float(input("boyunuzu giriniz: "))
+kilo = float(input("kilonuzu giriniz: "))
+
+bki = kilo/boy**2
+print("bki: ", format (bki, ".2f"))
+
+if bki<=18.49 :
+    print("zayıfsınız.")
+
+elif bki<=24.90 :
+    print("normal kilodasınız.")
+
+elif bki<=29.90 :
+    print("fazla kilolusunuz.")
+
+elif bki<=39.90 :
+     print("obezsiniz.")
+
+elif bki>=39.91 :
+     print("morbid obezsiniz")
+
+print("---SAĞLIKLI GÜNLER DİLERİZ---")
+
+
