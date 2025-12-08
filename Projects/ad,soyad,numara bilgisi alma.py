82dcfe1b61d5c3dd2feda92a56a868be35f223fc
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-08 22:27:03 +0300

    .

---
+++ Projects/ad,soyad,numara bilgisi alma.py
@@ -0,0 +1,8 @@
+print ("---sisteme hoş geldiniz---")
+ad = input("isminizi giriniz: ")
+soyad = input("soyadınızı giriniz: ")
+no = int(input("okul numaranızı giriniz: "))
+
+print("adınız:",ad, "\n soyisminiz:",soyad, "\n numaranız: ",no,)
+
+print ("---iyi günler---")
