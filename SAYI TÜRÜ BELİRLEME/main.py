d7afddffab2eb4bbedfc1f68ee745c73b31fbad4
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-12 17:44:39 +0300

    .

---
+++ SAYI TÜRÜ BELİRLEME/main.py
@@ -0,0 +1,14 @@
+print("---SİSTEME GOŞ GELDİNİZ---")
+
+sayı = int(input("sayı giriniz: "))
+
+if sayı>0:
+    print("sayınız pozitiftir")
+
+elif sayı<0:
+    print("sayınız negatiftir")
+
+else:
+    print("sayınız nötrdür")
+
+print("---İYİ GÜNLER DİLERİZ---")
