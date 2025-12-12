3abf51d80a3d4e4e1af99b6816a05b58bd74ea4c
onurkalinofficial <physicist.dr.onur@gmail.com>
2025-12-12 17:37:20 +0300

    .

---
+++ YAŞA GÖRE SİSTEME GİRİŞ/main.py
@@ -0,0 +1,10 @@
+
+yaş = int(input("Yaşınızı Giriniz: "))
+
+if yaş<18:
+    print("Sisteme girebilmek için 18 yaşında olmanız gerekmektedir. ")
+
+else:
+    print("Sisteme girişiniz yapılmaktadır...")
+
+    print("---SİSTEME HOŞ GELDİNİZ---")
